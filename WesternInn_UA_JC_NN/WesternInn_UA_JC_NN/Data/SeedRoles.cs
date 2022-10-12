a204d045c9dc19b0c3c32cf664b1c063981bc193
BigJMC9 <jake@crosbie.net.au>
2022-10-12 11:27:20 +1100

    Main Project
    The Main Project Repo

---
+++ WesternInn_UA_JC_NN/WesternInn_UA_JC_NN/Data/SeedRoles.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WesternInn_UA_JC_NN.Data
+{
+    public class SeedRoles
+    {
+
+        public static async Task CreateRoles(IServiceProvider serviceProvider, IConfiguration Configuration)
+        {
+            // Get the RoleManager and the UserManager objects
+            var RoleManager =
+            serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager =
+            serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+        }
+    }
+}
---
+++ WesternInn_UA_JC_NN/WesternInn_UA_JC_NN/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WesternInn_UA_JC_NN.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlite("Data Source=WesternInn.db"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+//add roles if needed
+builder.Services.AddDefaultIdentity<IdentityUser>().AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var serviceProvider = services.GetRequiredService<IServiceProvider>();
+        // get the Config object for the appsettings.json file
+        var configuration = services.GetRequiredService<IConfiguration>();
+        // Calling the static method created by us
+        SeedRoles.CreateRoles(serviceProvider, configuration).Wait();
+    }
+    catch
+    {
+
+    }
+}
